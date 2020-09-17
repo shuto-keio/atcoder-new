5f3563b00edc241764d168c32d64ce2fa49279cf
shuto-keio <shuto-keio@keio.jp>
2020-09-18 00:40:50 +0900

    add abc086 and submit abc

---
+++ abc086/c/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+
+t, x, y = 0, 0, 0
+for i in range(n):
+    t_new, x_new, y_new = list(map(int, input().split()))
+
+    time = t_new-t
+    dis = abs(x_new-x)+abs(y_new-y)
+
+    if time < dis:
+        print("No")
+        exit()
+    if time % 2 != dis % 2:
+        print("No")
+        exit()
+    t, x, y = t_new, x_new, y_new
+print("Yes")
---
+++ abc086/d/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(10**6)
+
+n, k = list(map(int, input().split()))
+
+X, Y, C = [], [], []
+for _ in range(n):
+    x, y, c = input().split()
+    X.append(int(x) % (2*k))
+    Y.append(int(y) % (2*k))
+    C.append(c == "B")
+G = [[0]*k for _ in range(k)]
+
+cnt = 0
+for x, y, c in zip(X, Y, C):
+    if (x >= k and y < k) or (x < k and y >= k):
+        x += k
+        c ^= 1
+    x %= k
+    y %= k
+    G[x][y] += (-1)**c
+    cnt += c
+# print(cnt)
+# print(G)
+
+
+for i in range(k):
+    for j in range(k-1):
+        G[i][j+1] += G[i][j]
+for j in range(k):
+    for i in range(k-1):
+        G[i+1][j] += G[i][j]
+# print(G)
+ans = 0
+for i in range(k):
+    for j in range(k):
+        temp = G[k-1][k-1] - G[i][k-1] - G[k-1][j] + G[i][j]*2 + cnt
+        # print(G[k-1][k-1], G[i][k-1], G[k-1][j], G[i][j]*2, cnt)
+        # print(temp, n-temp)
+        ans = max(ans, temp, n-temp)
+
+print(ans)
